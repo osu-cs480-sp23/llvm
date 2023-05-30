0086710bd0fb33e33f062b842d10e28e24c78b31
Rob Hess <hessro@oregonstate.edu>
2023-05-30 15:34:37 -0700

    Add code from 5/30 lecture.

--- compiler.c
+++ compiler.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <llvm-c/Core.h>
+
 #include "hash.h"
 
 int main() {
+    LLVMModuleRef module = LLVMModuleCreateWithName("foo.code");
+
+    LLVMTypeRef foo_type = LLVMFunctionType(
+        LLVMVoidType(), NULL, 0, 0
+    );
+    LLVMValueRef foo_fn = LLVMAddFunction(module, "foo", foo_type);
+
+    LLVMDisposeModule(module);
     return 0;
 }
